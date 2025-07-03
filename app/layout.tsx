290a3a146268eb9db97b1bc6d17f989a7d34ffee
Kamarudheen <kamaru.office@gmail.com>
2025-07-03 20:43:34 +0530

    fixed some bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Playfair_Display} from "next/font/google";
+import {Playfair_Display} from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const playfairDisplay = Playfair_Display({
   variable: "--font-playfair-display",
