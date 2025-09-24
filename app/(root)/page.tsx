be934f7618c0fdbe6288a1a203fe7402e1039f79
Kamarudheen <kamaru.office@gmail.com>
2025-09-24 21:18:14 +0530

    make site under maintanance

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,20 @@
-import HeroSection from "@/app/components/HeroSection";
-import WhatWeDo from "@/app/components/WhatWeDo";
-import Contact from "@/app/components/Contact";
-import Footer from "../components/Footer";
-import WhyChoose from "../components/WhyChoose";
+// import HeroSection from "@/app/components/HeroSection";
+// import WhatWeDo from "@/app/components/WhatWeDo";
+// import Contact from "@/app/components/Contact";
+// import Footer from "../components/Footer";
+// import WhyChoose from "../components/WhyChoose";
+
+import UnderMaintenance from "../components/UnderMaintenance";
 
 export default function Home() {
     return (
         <main className="no-vertical-scroll">
-            <HeroSection />
+            {/* <HeroSection />
             <WhatWeDo />
             <WhyChoose />
             <Contact />
-            <Footer />
+            <Footer /> */}
+            <UnderMaintenance />
         </main>
     );
 }
